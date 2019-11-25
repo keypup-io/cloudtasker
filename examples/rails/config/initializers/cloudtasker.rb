6414160a759bc46e0723cd43d92171f368b4eced
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2019-11-25 12:57:10 +0100

    update Rails example

--- examples/rails/config/initializers/cloudtasker.rb
+++ examples/rails/config/initializers/cloudtasker.rb
@@ -5,12 +5,25 @@ require 'cloudtasker/cron'
 require 'cloudtasker/batch'
 
 Cloudtasker.configure do |config|
-  config.secret = 'some-rails-secret'
-  # config.logger = Rails.logger
+  #
+  # GCP Configuration
+  #
   config.gcp_location_id = 'us-east1'
   config.gcp_project_id = 'some-project'
   config.gcp_queue_id = 'some-queue'
+
+  #
+  # Domain
+  #
+  # config.processor_host = 'https://xxxx.ngrok.io'
+  #
   config.processor_host = 'http://localhost:3000'
+
+  #
+  # Uncomment to process tasks via Cloud Task.
+  # Requires a ngrok tunnel.
+  #
+  # config.mode = :production
 end
 
 #
