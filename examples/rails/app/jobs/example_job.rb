88ada0f3055f1929c0672e10dfd47bb6fffaf45e
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-09-04 18:23:09 -0500

    Added example job on example rails app

---
+++ examples/rails/app/jobs/example_job.rb
@@ -0,0 +1,10 @@
+class ExampleJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    Rails.logger.info('Example job starting...')
+    Rails.logger.info(args.inspect)
+    sleep(3)
+    Rails.logger.info('Example job done!')
+  end
+end
