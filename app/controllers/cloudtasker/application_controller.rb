5b2e3e76fed553b0b795299d2afc6f9f24bede8b
Matthew Ford <matt@bitzesty.com>
2021-07-20 18:09:24 +0100

    do not raise skip verify_authenticity_token if not yet defined.

--- app/controllers/cloudtasker/application_controller.rb
+++ app/controllers/cloudtasker/application_controller.rb
@@ -3,6 +3,6 @@
 module Cloudtasker
   # Base Cloudtasker controller
   class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
   end
 end
