6d7eee8ccc2d045a42bf057d511c2b389d739fbc
John Morrison <john.morrison@1komma5grad.com>
2025-09-15 17:30:53 +0200

    add until-completed conflict strategy

---
+++ lib/cloudtasker/unique_job/lock/until_completed.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Cloudtasker
+  module UniqueJob
+    module Lock
+      # Conflict if any other job with the same args is scheduled or moved to execution
+      # while the first job is pending or executing. Unlocks only on successful completion
+      # or when a DeadWorkerError is raised.
+      class UntilCompleted < BaseLock
+        #
+        # Acquire a lock for the job and trigger a conflict
+        # if the lock could not be acquired.
+        #
+        def schedule(&block)
+          job.lock!
+          yield
+        rescue LockError
+          conflict_instance.on_schedule(&block)
+        end
+
+        #
+        # Acquire a lock for the job and trigger a conflict
+        # if the lock could not be acquired.
+        #
+        def execute(&block)
+          job.lock!
+          yield
+          # Unlock on successful completion
+          job.unlock!
+        rescue LockError
+          conflict_instance.on_execute(&block)
+        rescue Cloudtasker::DeadWorkerError
+          # Unlock when DeadWorkerError is raised
+          job.unlock!
+          raise
+        end
+      end
+    end
+  end
+end
